97375fd4c20aac0b863972ce0510112691806b35
Alessandro <asovitorio@gmail.com>
2020-08-11 20:56:34 -0300

    testeando o git

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-        <h1>Hallo Word!</h1>
+        <h1>Hallo Word ASlessannm!</h1>
         </p>
         <a
           className="App-link"
