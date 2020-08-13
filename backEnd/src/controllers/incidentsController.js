a39d285c79ff1dfbfbd3f330dcd4c52909a4adc4
Alessandro <asovitorio@gmail.com>
2020-08-12 23:55:16 -0300

    configurado login, create,delete logout

--- backEnd/src/controllers/incidentsController.js
+++ backEnd/src/controllers/incidentsController.js
@@ -2,24 +2,26 @@ const connection = require('../database/connection')
 const incidentsController = {
 
     index: async (req, res) => {
-        const { page = 1} = req.query
+        const {
+            page = 1
+        } = req.query
         const [count] = await connection('incidents').count();
-        res.header('pages',count['count(*)']);
+        res.header('pages', count['count(*)']);
         try {
             console.log(count['count(*)'])
             const incidents = await connection('incidents')
-            .join('ongs','ong_id','=','incidents.ong_id')
-            .limit(5)
-            .offset((page -1) * 5)
-            .select('incidents.*',
-            'ongs.name',
-            'ongs.email',
-            'ongs.whatsapp',
-            'ongs.city',
-            'ongs.uf');
+                .join('ongs', 'ong_id', '=', 'incidents.ong_id')
+                .limit(5)
+                .offset((page - 1) * 5)
+                .select('incidents.*',
+                    'ongs.name',
+                    'ongs.email',
+                    'ongs.whatsapp',
+                    'ongs.city',
+                    'ongs.uf');
             return res.status(200).json(incidents)
         } catch (error) {
-
+            return res.status(200).json(error)
         }
     },
     create: async (req, res) => {
@@ -27,43 +29,53 @@ const incidentsController = {
         // "description":"Voluntários para aulas de BD avançado",
         // "value":4.25,
         // "ong_id":"8b25eb3f"
-        const {title,description,value} = req.body;
-        const ong_id = req.headers.authorization
-
-        try {
-         const incidents = await connection('incidents').insert({
+        const {
             title,
             description,
-            value,
-            ong_id
-         })
-             
-              console.log(req.headers.authorization)
-              return res.status(201).json(incidents);
+            value
+        } = req.body;
+        const ong_id = req.headers.authorization
+        try {
+            const incidents = await connection('incidents').insert({
+                title,
+                description,
+                value,
+                ong_id
+            })
+
+            console.log(req.headers.authorization)
+            return res.status(201).json(incidents);
 
         } catch (error) {
             console.log(error)
             return res.status(400).json(error);
         }
     },
-    
-    delete: async (req,res) =>{
-        const {id} = req.params
+
+    delete: async (req, res) => {
+        const {
+            id
+        } = req.params
         const ong_id = req.headers.authorization
-          console.log(ong_id)
+        console.log(ong_id)
         try {
-            const incident = await connection('incidents').where('id',id).select('ong_id').first()
-        
+            const incident = await connection('incidents').where('id', id).select('ong_id').first()
+
             console.log(incident.ong_id)
-            if(incident.ong_id != ong_id){
-                return res.status(401).json({status:"Não Autorizado"})
+            if (incident.ong_id != ong_id) {
+                return res.status(401).json({
+                    status: "Não Autorizado"
+                })
             }
-          await connection('incidents').where('id',id).del()
-        return res.status(200).json({status:"Ok",id});
+            await connection('incidents').where('id', id).del()
+            return res.status(200).json({
+                status: "Ok",
+                id
+            });
         } catch (error) {
             return res.status(400).json(error);
         }
-        
+
     }
 }
 
